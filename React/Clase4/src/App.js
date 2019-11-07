aea757bf0245ff76767e2f99b132c8143f778933
Alisson Zamora <alison.zam18@gmail.com>
2019-11-07 02:52:42 -0600

    Add: New page

--- React/Clase4/src/App.js
+++ React/Clase4/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/home';
+import ShowCard from './pages/showCard';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path={'/'} page={HomePage} />
+          <Route path={'/'} page={HomePage} exact/>
+          <Route path={'/ShowCard'} page={ShowCard}  />
         </Switch>
       </BrowserRouter>
     </div>
